d9fa31629d7efb9d06a64eaf2e5bb28f5c785d4b
Olamide <olasupoolamide00@gmail.com>
2023-06-19 00:19:34 +0100

    create the header section

---
+++ src/components/MenuItem.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function MenuItems({title, address, Icon}) {
+  return (
+    <div>
+      <Link href={address} className="mx-4 lg:mx-6 hover:text-purple-700">
+        <Icon className="text-2xl mx-4 sm:hidden"/>
+        <p className="hidden sm:inline my-2 text-sm">{title}</p>
+      </Link>
+    </div>
+  );
+}
