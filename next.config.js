84e2180fb777789f8a0c5cee43768c730b02790b
Olamide <olasupoolamide00@gmail.com>
2023-06-23 19:13:22 +0100

    add card component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ["image.tmdb.org"],
+    },
+  };
+  
+  module.exports = nextConfig;
