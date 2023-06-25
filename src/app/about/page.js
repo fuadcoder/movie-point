d456bc0de4158b9a9c99e571edfbea04eafc6614
Olamide <olasupoolamide00@gmail.com>
2023-06-25 10:22:37 +0100

    edited the about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,26 +3,26 @@ export default function About() {
     <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-purple-500">About</h1>
       <p>
-        Welcome to <b>Movie Point</b>! We are a team of passionate movie
-        enthusiasts who have come together to create a one-stop destination for
-        all your movie-related needs and your ultimate destination for all
-        things cinema! We are passionate about movies and dedicated to providing
-        you with the latest news, in-depth reviews, and fascinating insights
-        into the world of filmmaking. Whether you're a casual moviegoer, a
-        die-hard fan, or a budding filmmaker yourself, we've got you covered.
+        Welcome to Movie Point! We are a team of passionate movie enthusiasts
+        who have come together to create a one-stop destination for all your
+        movie-related needs and your ultimate destination for all things cinema!
+        We are passionate about movies and dedicated to providing you with the
+        latest news, in-depth reviews, and fascinating insights into the world
+        of filmmaking. Whether you're a casual moviegoer, a die-hard fan, or a
+        budding filmmaker yourself, we've got you covered.
       </p>
 
       <p>
-        At <b>Movie Point</b>, we believe that movies have the power to
-        entertain, inspire, and transform us. They take us on incredible
-        journeys to far-off worlds, introduce us to captivating characters, and
-        immerse us in gripping narratives. Our mission is to celebrate the magic
-        of cinema and share our love for the art form with fellow movie
-        enthusiasts like you. Our website is designed to provide you with a
-        comprehensive database of movies from all around the world, along with
-        the latest news, reviews, and trailers. Our movie database is constantly
-        updated with new releases, ensuring that you have access to the latest
-        and greatest in the world of cinema. You can search for movies by title,
+        At Movie Point, we believe that movies have the power to entertain,
+        inspire, and transform us. They take us on incredible journeys to
+        far-off worlds, introduce us to captivating characters, and immerse us
+        in gripping narratives. Our mission is to celebrate the magic of cinema
+        and share our love for the art form with fellow movie enthusiasts like
+        you. Our website is designed to provide you with a comprehensive
+        database of movies from all around the world, along with the latest
+        news, reviews, and trailers. Our movie database is constantly updated
+        with new releases, ensuring that you have access to the latest and
+        greatest in the world of cinema. You can search for movies by title,
         director, actor, genre, or release date, making it easy to find the
         perfect movie for any occasion.
       </p>
@@ -46,7 +46,7 @@ export default function About() {
         Join us at Movie Point, where the magic of cinema comes alive! If you
         have any feedback or suggestions, please feel free to contact us. We are
         always looking for ways to improve and enhance the user experience on
-        our website. <b>Movie Point</b> - Celebrating the Art of Film.
+        our website. Movie Point - Celebrating the Art of Film.
       </p>
     </div>
   );
