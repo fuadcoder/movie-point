91d5a3c3c73310e10e52568802a8bbd7144aef8f
Olamide <olasupoolamide00@gmail.com>
2023-06-27 17:12:37 +0100

    fix the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,7 +15,7 @@ export default function Card({ result }) {
           className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
           style={{
             maxWidth: "100%",
-            height: "150px",
+            height: "auto",
           }}
           placeholder="blur"
           blurDataURL="/spinner.svg"
